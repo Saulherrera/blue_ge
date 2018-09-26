83b7146197d7ba5f50e1891b67303901c908df80
Felipe Tascon <tascon17@gmail.com>
2018-09-26 20:29:15 +0200

    fix about and courses page

---
+++ examples.php
@@ -0,0 +1,55 @@
+<div class="col-sm-12 latam">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Gestor Energético en Latinoamérica</h3>
+        </div>
+        <div class="col-sm-6">
+            <img src="http://practicas.gestor-energetico.com/site/wp-content/uploads/2018/09/mapa.png" alt=""
+                 width="358" height="453" class="alignnone size-full wp-image-206"/>
+        </div>
+        <div class="col-sm-6">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="http://practicas.gestor-energetico.com/site/wp-content/uploads/2018/09/2_.png" alt=""
+                         width="143" height="144" class="alignnone size-full wp-image-205"/>
+                </div>
+                <div class="col-sm-6">
+                    <p>
+                        <strong>Perú</strong>
+                        <strong>Sandra Barrantes</strong>
+                        <span>Arquitecta</span>
+                        <span>LEED AP - EDGE Expert</span>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="http://practicas.gestor-energetico.com/site/wp-content/uploads/2018/09/1_mariana.png"
+                         alt="" width="143" height="144" class="alignnone size-full wp-image-203"/>
+                </div>
+                <div class="col-sm-6">
+                    <p>
+                        <strong>Máxico</strong>
+                        <strong>Mariana Aristizabal</strong>
+                        <span>LEED AP Ing. Electrónica</span>
+                        <span>Magister en energías renovables</span>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="http://practicas.gestor-energetico.com/site/wp-content/uploads/2018/09/1_pablo.png" alt=""
+                         width="143" height="144" class="alignnone size-full wp-image-204"/>
+                </div>
+                <div class="col-sm-6">
+                    <p>
+                        <strong>Chile</strong>
+                        <strong>Pablo Pulgar</strong>
+                        <span>Arquitecto LEED</span>
+                        <span>BIM Manager</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
